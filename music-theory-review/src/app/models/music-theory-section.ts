7a01452b92b021624c1f49947cb3d8acf7d0bef2
LAPTOP-5I1H9LOK\river <chrisrice1188@gmail.com>
2022-12-01 09:32:17 -0700

    Updated classes

---
+++ music-theory-review/src/app/models/music-theory-section.ts
@@ -0,0 +1,11 @@
+export abstract class MusicTheorySection {
+    
+}
+
+export class TextSection extends MusicTheorySection {
+    
+}
+
+export class VideoSection extends MusicTheorySection {
+    
+}
---
+++ music-theory-review/src/app/models/music-theory.ts
@@ -0,0 +1,14 @@
+import { MusicTheorySection, TextSection, VideoSection } from "./music-theory-section"
+
+class MusicTheory {
+    title?: string
+    description?: string
+    image?: string
+    musicTheorySections: MusicTheorySection[] = []
+}
+
+
+// let x: String = new String('text');
+// let y: Boolean = new Boolean(false);
+// let z: MusicTheory = new MusicTheory();
+// z.musicTheorySections.push(new TextSection());
